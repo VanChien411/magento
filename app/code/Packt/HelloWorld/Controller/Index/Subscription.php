b5d7f85448dc3e585e4368d6c81d4d81b71cd4a1
VanChien411 <phuthienchien3@gmail.com>
2025-04-21 16:40:04 +0700

    Add Subscription controller for handling user subscriptions

---
+++ app/code/Packt/HelloWorld/Controller/Index/Subscription.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Packt\HelloWorld\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
+
+class Subscription extends Action
+{
+    protected $_objectManager;
+
+    public function __construct(Context $context)
+    {
+        parent::__construct($context);
+        $this->_objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+    }
+
+    public function execute()
+    {
+        $subscription = $this->_objectManager->create('Packt\HelloWorld\Model\Subscription');
+        $subscription->setFirstname('John');
+        $subscription->setLastname('Doe');
+        $subscription->setEmail('john.doe@example.com');
+        $subscription->setMessage('A short message to test');
+        $subscription->save();
+
+        // Cách trả về thông báo kiểu Raw
+        /** @var \Magento\Framework\Controller\Result\Raw $resultRaw */
+        $resultRaw = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $resultRaw->setContents('success');
+        return $resultRaw;
+    }
+}
