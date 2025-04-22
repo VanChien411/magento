08f5044d728b4a5065b67d17c637c84e72c25247
VanChien411 <phuthienchien3@gmail.com>
2025-04-22 11:30:57 +0700

    Add Collection controller to manage product collections and set prices

---
+++ app/code/Packt/HelloWorld/Controller/Index/Collection.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Packt\HelloWorld\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Response\Http;
+
+class Collection extends Action
+{
+    public function execute()
+    {
+        $productCollection = $this->_objectManager
+            ->create('Magento\Catalog\Model\ResourceModel\Product\Collection')
+            ->addAttributeToSelect('*')
+            // ->addAttributeToFilter('entity_id', array(
+            //     'in' => array(159, 160, 161)
+            // ))
+            // ->addAttributeToFilter(
+            //     'name',
+            //     ['like' => '%Sport%']
+
+            // )
+            ->addAttributeToFilter('name', 'Áo thun');
+
+        // Gán giá trị cho tất cả các sản phẩm trong collection
+        $productCollection->setDataToAll('price', 20);
+
+
+        $output = '';
+        foreach ($productCollection as $product) {
+            $product->getResource()->saveAttribute($product, 'price');
+            $output .= nl2br(print_r($product->getData(), true));
+        }
+        // $output = $productCollection->getSelect()->__toString();
+        // In ra câu lệnh SQL
+        /** @var Http $response */
+        $response = $this->getResponse();
+        $response->setHeader('Content-Type', 'text/plain', true);
+        $response->setBody($output);
+    }
+}
