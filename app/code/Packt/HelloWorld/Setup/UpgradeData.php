0159f4955932dcc9f817ef3577f18fe501dfb461
VanChien411 <phuthienchien3@gmail.com>
2025-04-24 11:14:34 +0700

    Update UpgradeData and module version to 2.0.2; fix attribute group name and clean up constructor

--- app/code/Packt/HelloWorld/Setup/UpgradeData.php
+++ app/code/Packt/HelloWorld/Setup/UpgradeData.php
@@ -3,32 +3,31 @@ namespace Packt\HelloWorld\Setup;
 use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Catalog\Setup\CategorySetupFactory;
 class UpgradeData implements UpgradeDataInterface {
 protected $categorySetupFactory;
-
-public function __construct
-(\Magento\Catalog\Setup\CategorySetupFactory
-$categorySetupFactory) {
+public function __construct(CategorySetupFactory $categorySetupFactory) {
 $this->categorySetupFactory = $categorySetupFactory;
 }
 public function upgrade(ModuleDataSetupInterface $setup,
 ModuleContextInterface $context) {
-if (version_compare($context->getVersion(), '2.0.2') < 0) 
-{
-    $categorySetup = $this->categorySetupFactory->create(['setup' => $setup]);
-    $entityTypeId = $categorySetup->getEntityTypeId(\Magento\Catalog\Model\Product::ENTITY);
-    $categorySetup->addAttribute($entityTypeId,
-        'helloworld_label', array(
-        'type' => 'varchar',
-        'label' => 'HeloWorld label',
-        'input' => 'text',
-        'required' => false,
-        'visible_on_front' => true,
-        'apply_to' =>
-        'simple,configurable,virtual,bundle,downloadable',
-        'unique' => false,
-        'group' => 'HelloWorld'
-    ));
+if (version_compare($context->getVersion(), '2.0.3') <
+0) {
+$categorySetup = $this->categorySetupFactory->create(['setup' => $setup]);
+$entityTypeId = $categorySetup->getEntityTypeId
+(\Magento\Catalog\Model\Product::ENTITY);
+$categorySetup->addAttribute($entityTypeId,
+'helloworld_label', array(
+'type' => 'varchar',
+'label' => 'HeloWorld label',
+'input' => 'text',
+'required' => false,
+'visible_on_front' => true,
+'apply_to' =>
+'simple,configurable,virtual,bundle,downloadable',
+'unique' => false,
+'group' => 'HelloWorld1'
+));
 }
 }
 }
